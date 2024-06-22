3de705bace83bb788d5f825db613ba209247520b
vwh <vwhe@proton.me>
2024-06-22 14:15:00 +0300

    make it ready for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/python-playground/",
   plugins: [react()],
   resolve: {
     alias: {
