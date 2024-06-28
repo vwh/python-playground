6cf399864cdcae3370983e0e7f7397a5e89ef785
vwh <vwhe@proton.me>
2024-06-29 00:52:54 +0300

    make it PWA ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/python-playground/",
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
